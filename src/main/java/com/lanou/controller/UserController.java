f20076fd5fdfd595292effb8741b2dd7d8405bb7
xuxiaofeng <2641423174@qq.com>
2018-07-26 11:56:49 +0800

    controller.java

---
+++ src/main/java/com/lanou/controller/UserController.java
@@ -0,0 +1,11 @@
+package com.lanou.controller;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by xuxiaofeng on 2018/7/26.
+ */
+@Controller
+public class UserController {
+
+}
